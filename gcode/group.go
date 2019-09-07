e11f3d43510107b14c46219427f981dd65fc72de
McMahon, Ian <ian.mcmahon@delta.com>
2019-09-07 02:41:01 -0400

    Implement statement parser
    The statement parser is simple, it splits a line into code and comment
    It then uses two regular expressions to pull out the command and all
    parameters, then stores those parameters in a map.
    
    I also implemented the Group type, which classifies g-code statements by
    NIST-standard groups (such as MOTION).
    
    I implemented the Statement object with a couple of example methods:
    Group() returns the statement's Group, IsMotion() is a boolean check of
    Group().  Moved(axis) returns the value of that axis parameter if the
    statement is a motion command.
    
    I implemented two test cases which exercise parsing, summing E moves
    across a list of statements, and verifying group classification.

---
+++ gcode/group.go
@@ -0,0 +1,88 @@
+package main
+
+import "sync"
+
+/*
+	This is some syntactic sugar for classifying commands into established groups.
+	This comes from https://github.com/synthetos/g2/wiki/GCode-Parsing, who purports
+	to have taken it from the NIST standard.
+
+	I added T0-T4, it's a bit goofy because they're not "real" g-code commands,
+	and they are the only command where the number is a parameter.
+
+	How this works is simple, I'm creating a type called Group which is just an alias
+	for int.  The const section declares all the Group constants, and numbers them
+	from zero up.  groupMemberships maps each group to a list of commands in the group.
+
+	The Group() method is bound to Statement, and it runs a function which initializes
+	a reverse map based on the groupMemberships map.  This is run via a sync.Once object,
+	which ensures that the initialization only happens the first time it's called on any
+	Statement object.  This is thread safe, although I don't expect concurrency in this.
+
+	This initialization could have also been done in func init(), which would happen at
+	startup.  I used sync.Once because I like the lazy loading aspect.
+*/
+
+type Group int
+
+const (
+	UNKNOWN Group = iota
+	NON_MODAL
+	MOTION
+	PLANE_SELECTION
+	DISTANCE_MODE
+	FEED_RATE_MODE
+	UNITS
+	CUTTER_RAD_COMP
+	TOOL_LENGTH_OFFSET
+	RETURN_MODE
+	CS_SEL
+	PATH_CONTROL_MODE
+	STOPPING
+	TOOLCHANGE
+	SPINDLE_TURNING
+	COOLANT
+	FRO_ENABLE
+)
+
+var groupMemberships = map[Group][]string{
+	// non-modals
+	NON_MODAL: []string{"G4", "G10", "G28", "G30", "G53", "G92", "G92.1", "G92.2", "G92.3"},
+	// G groups
+	MOTION:             []string{"G0", "G1", "G2", "G3", "G38.2", "G80", "G81", "G82", "G83", "G84", "G85", "G86", "G87", "G88", "G89"},
+	PLANE_SELECTION:    []string{"G17", "G18", "G19"},
+	DISTANCE_MODE:      []string{"G90", "G91"},
+	FEED_RATE_MODE:     []string{"G93", "G94"},
+	UNITS:              []string{"G20", "G21"},
+	CUTTER_RAD_COMP:    []string{"G40", "G41", "G42"},
+	TOOL_LENGTH_OFFSET: []string{"G43", "G49"},
+	RETURN_MODE:        []string{"G98", "G99"},
+	CS_SEL:             []string{"G54", "G55", "G56", "G57", "G58", "G59", "G59.1", "G59.2", "G59.3"},
+	PATH_CONTROL_MODE:  []string{"G61", "G61.1", "G64"},
+
+	// M group
+	STOPPING:        []string{"M0", "M1", "M2", "M30", "M60"},
+	TOOLCHANGE:      []string{"M6", "T0", "T1", "T2", "T3"}, // this is hacky, T commands aren't part of the gcode spec, and it's the only command where the number is a parameter
+	SPINDLE_TURNING: []string{"M3", "M4", "M5"},
+	COOLANT:         []string{"M7", "M8", "M9"}, // special case: M7 and M8 may be active at the same time
+	FRO_ENABLE:      []string{"M48", "M49"},
+}
+
+var once sync.Once
+var commandToGroup map[string]Group
+
+func (s *Statement) Group() Group {
+	once.Do(func() {
+		commandToGroup = make(map[string]Group, 0)
+		for group, commands := range groupMemberships {
+			for _, command := range commands {
+				commandToGroup[command] = group
+			}
+		}
+	})
+
+	if group, ok := commandToGroup[s.command]; ok {
+		return group
+	}
+	return UNKNOWN
+}
---
+++ gcode/statement_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+type testLine struct {
+	line          string
+	expectedGroup Group
+}
+
+// set up a list of test lines to parse, along with metadata about what to assert in the tests
+var testLines = []testLine{
+	{"M0", STOPPING},
+	{"T0", TOOLCHANGE},
+	{"G4 S0 ; Dwell", NON_MODAL},
+	{"G1  Y142.400", MOTION},
+	{"G1 Z0.40 F10800", MOTION},
+	{"; --- P2PP Set wipe speed to 2000.0mm/s", UNKNOWN},
+	{"G1 F4000.0", MOTION},
+	{"G0 X230.082 Y142.4", MOTION},
+	{"; CP TOOLCHANGE WIPE", UNKNOWN},
+	{"G1  X181.000 E1.8654 F1600", MOTION},
+	{"G1  X180.250 Y142.900  E-0.0343", MOTION},
+	{"G1  X239.000 E2.2329 F1800", MOTION},
+}
+
+func TestParseStatement(t *testing.T) {
+	E := 0.0
+	for _, line := range testLines {
+		stmt := parseStatement(line.line)
+		fmt.Printf("%s\n", stmt)
+		E += stmt.Moved("E")
+	}
+
+	fmt.Printf("Total E travel: %.4f\n", E)
+	if E != 4.064 {
+		t.Errorf("test code E travel summed to %.4f, expected %.4f", E, 4.064)
+	}
+}
+
+func TestGroupClassification(t *testing.T) {
+	for _, line := range testLines {
+		stmt := parseStatement(line.line)
+		if stmt.Group() != line.expectedGroup {
+			t.Errorf("%s should classify as group %v, but was group %v instead", stmt, stmt.Group(), line.expectedGroup)
+		}
+	}
+}
