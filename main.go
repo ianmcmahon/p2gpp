31c05ee83c1593b262fd076350f0088ec999d066
McMahon, Ian <ian.mcmahon@delta.com>
2019-09-07 13:10:54 -0400

    Update example to match parser

--- main.go
+++ main.go
@@ -8,5 +8,19 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%#v\n", gcode)
+	fmt.Printf("startCode:\n")
+	for _, stmt := range gcode.startCode.Lines() {
+		fmt.Printf("\t%#v\n", stmt)
+	}
+
+	for i, layer := range gcode.layers {
+		fmt.Printf("layer %d:\n", i)
+		for b, block := range layer.blocks {
+			fmt.Printf("\tblock %d:\n", b)
+			for _, stmt := range block.Lines() {
+				fmt.Printf("\t\t%#v\n", stmt)
+			}
+		}
+	}
+	fmt.Printf("endCode: %#v\n", gcode.endCode)
 }
